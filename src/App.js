e4372f03e6dceb222c5527fabb8731374f36d5e8
wildcard329 <enkiduwildman@gmail.com>
2020-03-23 13:37:10 -0700

    set up project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
+import {fetchShow} from "./components/api/fetchShow";
 
 import { formatSeasons } from "./utils/formatSeasons";
 
@@ -15,18 +15,11 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
-  }, []);
+    fetchShow()
+      .then(res =>{
+        setShow(res.data);
+        setSeasons(formatSeasons(res.data._embedded.episodes))})
+  })
 
   const handleSelect = e => {
     setSelectedSeason(e.value);
@@ -51,3 +44,19 @@ export default function App() {
     </div>
   );
 }
+
+
+
+// useEffect(() => {
+//   const fetchShow = () => {
+//     axios
+//       .get(
+//         "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+//       )
+//       .then(res => {
+//         setShow(res.data);
+//         setSeasons(formatSeasons(res.data._embedded.episodes));
+//       });
+//   };
+//   fetchShow();
+// }, []);
